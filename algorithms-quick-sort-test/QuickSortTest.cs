8251d31c7ca8b4f39a0bf80397990970b4e95bf4
Guilherme Ramos Mendes <guilherme.rmendes95@gmai.com>
2021-06-26 22:37:18 -0300

    Implementing test

---
+++ algorithms-quick-sort-test/QuickSortTest.cs
@@ -0,0 +1,19 @@
+using algorithms_quick_sort.Sort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algorithms_quick_sort_test
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void ShouldSortArrayWithQuickSortAlgorithm()
+        {
+            int[] values = new int[] { 7, 8, 1, 2, 90, 4, 65, 32 };
+            QuickSort.Execute(values);
+            Assert.AreEqual(1, values[0]);
+            Assert.AreEqual(2, values[1]);
+            Assert.AreEqual(4, values[2]);
+        }
+    }
+}
