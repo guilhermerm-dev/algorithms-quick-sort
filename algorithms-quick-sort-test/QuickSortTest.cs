db9e07c289b0a61a5c2861edc2090859c9dbcdec
Guilherme Ramos Mendes <guilherme.rmendes95@gmai.com>
2021-06-26 23:58:00 -0300

    Adding more tests

--- algorithms-quick-sort-test/QuickSortTest.cs
+++ algorithms-quick-sort-test/QuickSortTest.cs
@@ -7,13 +7,23 @@ namespace algorithms_quick_sort_test
     public class QuickSortTest
     {
         [TestMethod]
-        public void ShouldSortArrayWithQuickSortAlgorithm()
+        public void ShouldSortArrayWithQuickSortAlgorithm1()
         {
             int[] values = new int[] { 7, 8, 1, 2, 90, 4, 65, 32 };
             QuickSort.Execute(values);
             Assert.AreEqual(1, values[0]);
             Assert.AreEqual(2, values[1]);
             Assert.AreEqual(4, values[2]);
+        }
+
+                [TestMethod]
+        public void ShouldSortArrayWithQuickSortAlgorithm2()
+        {
+            int[] values = new int[] { 8, 3, 2, 4, 1, 9, 10, 11 };
+            QuickSort.Execute(values);
+            Assert.AreEqual(1, values[0]);
+            Assert.AreEqual(2, values[1]);
+            Assert.AreEqual(3, values[2]);
         }
     }
 }
